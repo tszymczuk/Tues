f172a5d3442b87cd9dd9ca2d536561120b15e1d3
Tom Szymczuk <tszymczuk@student.bridgew.edu>
2021-09-16 10:34:00 -0400

    BankAccount

---
+++ src/com/company/BankAccount.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class BankAccount {
+    private double balance;
+    private float interestRate;
+
+    public void deposit(double amount){
+        balance += amount;
+    }
+
+    public boolean withdraw(double amount){
+        if (amount > balance){
+            return false;
+        }
+        balance = balance - amount;
+        return true;
+    }
+
+    public double checkBalance(){
+        return 0;
+    }
+
+
+
+}
