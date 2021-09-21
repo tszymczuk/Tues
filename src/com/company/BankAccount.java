05b18538b59178f033fa2385d881b13dd29ec02e
Tom Szymczuk <tszymczuk@student.bridgew.edu>
2021-09-21 10:30:26 -0400

    BankAccount

--- src/com/company/BankAccount.java
+++ src/com/company/BankAccount.java
@@ -3,7 +3,25 @@ package com.company;
 public class BankAccount {
     private double balance;
     private float interestRate;
+    private int accountID;
+    private static int nextID = 100;
 
+
+    public BankAccount(){
+        interestRate = 0.02f;
+        accountID = nextID;
+        nextID++; //Same as nextID = nextID + 1
+    }
+    public BankAccount(double intialBalance, float initialRate){
+        balance = intialBalance;
+        interestRate = initialRate;
+        accountID = nextID;
+        nextID += 1;
+    }
+
+    public int getAccountID() {
+        return accountID;
+    }
     public void deposit(double amount){
         balance += amount;
     }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,13 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
         var myAccount = new BankAccount();
+        var yourAccount = new BankAccount(2000, 0.05f);
         myAccount.deposit(1000);
         var newBalance = myAccount.addInterest();
-
+        var yourBalance = yourAccount.addInterest();
+        System.out.println("Your account with account ID " + yourAccount.getAccountID() + " has $" + yourBalance +  " adding interest.");
         var succeeded = myAccount.withdraw(2000);
         if (succeeded)
             System.out.println("You managed to withdraw successfully ");
         else
-            System.out.println("You tried to withdraw too much, your balance is " +myAccount.checkBalance());
+            System.out.println("You tried to withdraw too much, your balance is $" + myAccount.checkBalance() + " for account ID " + myAccount.getAccountID());
     }
 }
