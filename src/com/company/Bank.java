0981d753ae42d35260607aacfeb85d8942342eaa
Tom Szymczuk <tszymczuk@student.bridgew.edu>
2021-09-23 11:17:12 -0400

    Adding menu items

--- src/com/company/Bank.java
+++ src/com/company/Bank.java
@@ -24,11 +24,22 @@ public class Bank {
                 case 2:
                     addCustomer(menuReader);
                     break;
+                case 3:
+                    Customer current = selectCustomer(menuReader);
+                    break;
+                default:
+                    System.out.println("You entered an incorrect menu option");
             }
         }
     }
 
+    private Customer selectCustomer(Scanner reader) {
+
+    }
+
+
     private void addCustomer(Scanner inputReader) {
+        inputReader.nextLine(); //Eat the orphan newLine from previous nextInt call
         System.out.print("What is the new Customer's name?: ");
         var customerName = inputReader.nextLine();
         System.out.print("What is the new Customer's Tax ID (SSN): ");
@@ -37,11 +48,13 @@ public class Bank {
         allCustomers.add(newCustomer);
     }
 
+    }
+
     private void printMenu() {
         System.out.println("What would you like to do next? (Select the number):");
         System.out.println("      [1] Exit the Program");
         System.out.println("      [2] Add a Customer");
-        System.out.println("      We'll add more here soon");
+        System.out.println("      [3] Select Customer by ID");
         System.out.print(" Enter Choice: ");
     }
 }
