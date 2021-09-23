b4d5a21d090284b6a3fe811991addd6fe9382b91
Tom Szymczuk <tszymczuk@student.bridgew.edu>
2021-09-23 10:34:12 -0400

    Creating Bank

--- src/com/company/Bank.java
+++ src/com/company/Bank.java
@@ -1,10 +1,40 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Bank {
     private ArrayList<BankAccount> allAccounts;
-    private ArrayList<Customer> allCustomer;
+    private ArrayList<Customer> allCustomers;
+
+    public Bank(){
+        allAccounts = new ArrayList<BankAccount>();
+        allCustomers = new ArrayList<Customer>();
+    }
 
 
+    public void doBanking(){
+        var menuReader = new Scanner(System.in);
+        while(true){
+            printMenu();
+            var userChoice = menuReader.nextInt();
+            switch (userChoice){
+                case 1:
+                    System.exit(0);
+                case 2:
+                    addCustomer();
+                    break;
+            }
+        }
+    }
+
+    private void addCustomer() {
+    }
+
+    private void printMenu() {
+        System.out.println("What would you like to do next? (Select the number):");
+        System.out.println("      [1] Exit the Program");
+        System.out.println("      [2] Add a Customer");
+        System.out.println("      We'll add more here soon");
+    }
 }
